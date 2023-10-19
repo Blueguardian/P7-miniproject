95c02fcb9196da680e7428f0005d468efce9c782
Adrián Sanchis Reig <63638774+Adriagent@users.noreply.github.com>
2023-10-19 12:36:06 +0200

    Add files via upload
    In the end I left ransac function as an indepenent file.
    
    Ransac funcion improved:
    Now it is calculated using np matrix operations, being much faster than before, when it was calculated using a nested loop.

--- mini_project.py
+++ mini_project.py
@@ -1,18 +1,19 @@
-import cv2
-from ransac import ransac
+import cv2, numpy as np, random
+from ransac import ransac_homography
+
 
 img1 = cv2.imread("media2/img1.jpg")
 img2 = cv2.imread("media2/img2.jpg")
 
-img1=cv2.resize(img1,None, fx=0.4, fy=0.4)
-img2=cv2.resize(img2,None, fx=0.4, fy=0.4)
+scale = 500/img1.shape[0]
+img1=cv2.resize(img1,None, fx=scale, fy=scale)
+img2=cv2.resize(img2,None, fx=scale, fy=scale)
 
 img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 # SIFT keypoints/descriptors:
 sift = cv2.SIFT_create()
-
 kp_1, des_1 = sift.detectAndCompute(img1_gray,None)
 kp_2, des_2 = sift.detectAndCompute(img2_gray,None)
 
@@ -29,26 +30,35 @@ for m, n in matches:
 # Filtering keypoints:
 matches = [kp_1[m[0].queryIdx].pt + kp_2[m[0].trainIdx].pt for m in good]
 
-matched_image = cv2.drawMatchesKnn(img1, kp_1, img2, kp_2, good, None)
+# Showing both images with matches and correspondences
+matched_image = cv2.drawMatchesKnn(img1, kp_1, img2, kp_2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 cv2.imshow("matched_image", matched_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-# a partir de aqui hacemos el ransac
-h = ransac(matches, tolerance=3, max_iterations=500, threshold=0.9)
+# Here we calculate H with ransac:
+h = ransac_homography(matches, n_samples=4, tolerance=1, max_iterations=5000, threshold=0.9)
 
-
+# Warping image 1 to stich it later.
 img_warped = cv2.warpPerspective(img1, h, (img2.shape[1],img2.shape[0]))
 
+alpha = 0.4
+result = cv2.addWeighted(img_warped, alpha, img2, 1-alpha, 0)
 
-# Print transformation matrix and images.
-print("\n",h,"\n")
 
-cv2.imshow("img1", img1)
-cv2.imshow("img2", img2)
-cv2.imshow("img1 warped", img_warped)
+# Putting together all the images so they can be shown with just one winow
+hor1 = np.concatenate((img1, img2), axis=1) 
+hor2 = np.concatenate((img_warped, result), axis=1) 
+subplot = np.concatenate((hor1,hor2), axis=0)
+
+subplot=cv2.resize(subplot,None, fx=0.75, fy=0.75)
+cv2.imshow("blend of warpe  d + img2", subplot)
+
+# Creating panoramic image with img1 + img2.
+result2 = cv2.warpPerspective(img1, h, (img1.shape[1] + img2.shape[1], int(img1.shape[0]*1.25)))
+result2[0:img2.shape[0], 0:img2.shape[1]] = img2
+cv2.imshow("Panoramic img1 + img2", result2)
+
 cv2.waitKey(0)
-
-
 cv2.destroyAllWindows()
--- ransac.py
+++ ransac.py
@@ -1,45 +1,73 @@
-import cv2, random, numpy as np
+import numpy as np, random
 
-def ransac(matches, tolerance, max_iterations, threshold):
+def get_homography(src, dst):
+
+    A = []
+    N = len(src) # A matrix can be ajusted for N_points >= 4.
+
+    for i in range(N):
+        x, y = src[i][0], src[i][1]
+        xp, yp = dst[i][0], dst[i][1]
+        A.append([x, y, 1, 0, 0, 0, -x * xp, -xp * y, -xp])
+        A.append([0, 0, 0, x, y, 1, -yp * x, -yp * y, -yp])
+
+    A = np.asarray(A)
+
+    U, S, Vh = np.linalg.svd(A)
+    L = Vh[-1, :] / Vh[-1, -1]
+
+    H = L.reshape(3, 3)
+    H /= H[2, 2]            # Normalization step. H is [3, 3] matrix.
+
+    return H
+
+def ransac_homography(matches, n_samples=4, tolerance=2, max_iterations=500, threshold=1):
 
     best_inliers = 0
-    best_H = np.eye(3,3)
+    best_H = np.eye(3)
+    best_MSE = np.inf
+
+    # separate source points from destination points.
+    matches_src, matches_dst = np.split(np.array(matches).transpose(), 2, 0)     
+    matches_src = np.insert(matches_src,2,1,axis=0)     # Adding 1 at the end and reshaping to columns instead of rows.
+    matches_dst = np.insert(matches_dst,2,1,axis=0) 
+
 
     for i in range(max_iterations):
 
-        samples = np.array(random.sample(matches, 4))
+        # First we get n random samples from all the matches.
+        samples = np.array(random.sample(matches, n_samples))
+        samp_src, samp_dst = np.split(samples, 2, 1)
 
-        samp_src = samples[:,:2]
-        samp_dst = samples[:, 2:]
+        H = get_homography(samp_src,samp_dst)   # Obtaining the Homography matrix.                        
 
-        H, _ = cv2.findHomography(samp_src, samp_dst, 0) # We will have to change this and do it from scratch.
+        # Proyecting all the source points into the destination space using the Homography.
+        src_proy = np.dot(H,matches_src)
 
-        inliers=0
+        # Obtaining the distances between all the proyected points and their correspondences. 
+        distances = np.linalg.norm(matches_dst-src_proy,axis=0)
 
-        for pair in matches:
-
-            p1 = np.append(pair[:2],1).reshape(3,1)
-            p2 = np.append(pair[2:],1).reshape(3,1)
-
-            p2_aprox = np.dot(H,p1)
-
-            dist = np.linalg.norm(p2 - p2_aprox)
-
-            if dist < tolerance:
-                inliers += 1
-
-
-        if inliers > best_inliers:
-            best_inliers = inliers
+        # Now we the number of inliers for this H and the Mean Squared Error of the distances between matches.
+        error_inliers = distances[distances<=tolerance]
+        n_inliers = len(error_inliers)
+        if n_inliers:
+            mse = np.square(error_inliers).mean()
+        else:
+            mse = np.inf
+        
+        if n_inliers > best_inliers or (n_inliers == best_inliers and mse < best_MSE):
+            best_inliers = n_inliers
             best_H = H
+            best_MSE = mse
 
-        if best_inliers/len(matches) >= threshold: 
-            print(f"n of iterations: {i+1}")
+        if (i+1) % 500 == 0:
+            print(f"-> i: {i+1} \tbest: [{best_inliers}/{len(matches)}] \tMSE: {best_MSE:.3f}")
+
+        if best_inliers/len(matches) >= threshold:
             break
 
-        
-        #print(f"{inliers}/{len(matches)}: \tbest: {best_inliers}")
 
-    print(f"\n[{best_inliers}/{len(matches)}]: {best_inliers/len(matches)}% ratio")
+    print(f"\n[#]: n of iterations done: {i+1}.")
+    print(f"[#] Result: {best_inliers}/{len(matches)}: {best_inliers/len(matches):.3f} ratio. MSE: {best_MSE:.3f}\n")
 
     return best_H
