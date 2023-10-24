fc16e8593b97acafc809732452ad35b1f653245b
Adrián Sanchis Reig <63638774+Adriagent@users.noreply.github.com>
2023-10-24 12:43:23 +0200

    Add files via upload
    - Error with Ransac function fixed: The projected points weren't scaled, affecting the final result.
    - Added media 5 and 6 which are sets of more than 2 images.
    - Now the project can stitch more than 2 images and create a merged image.
    - Function for getting the MSE and number of inliers from cv2.findHomography added for future benchmarks with our methods.
    - Homography refinement added by calculating the homography with all the inliers obtained with ransac instead of just using 4 pairs of points.
    - Several minor changes to the code for clarity

--- mini_project.py
+++ mini_project.py
@@ -1,64 +1,139 @@
-import cv2, numpy as np, random
+import cv2, numpy as np
+from os import listdir
 from ransac import ransac_homography
 
 
-img1 = cv2.imread("media2/img1.jpg")
-img2 = cv2.imread("media2/img2.jpg")
+def read_images(file):
 
-scale = 500/img1.shape[0]
-img1=cv2.resize(img1,None, fx=scale, fy=scale)
-img2=cv2.resize(img2,None, fx=scale, fy=scale)
+    image_names = listdir(file)
 
-img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    images = []
 
-# SIFT keypoints/descriptors:
-sift = cv2.SIFT_create()
-kp_1, des_1 = sift.detectAndCompute(img1_gray,None)
-kp_2, des_2 = sift.detectAndCompute(img2_gray,None)
+    for name in image_names:
+        img = cv2.imread(file+name)
 
-# Matching keypoints:
-matcher = cv2.BFMatcher()
-matches = matcher.knnMatch(des_1, des_2, k=2)
+        scale = 500/img.shape[0]
+        img=cv2.resize(img,None, fx=scale, fy=scale)
 
-# Filtering matches:
-good = []
-for m, n in matches:
-    if m.distance < 0.75 * n.distance:
-        good.append([m])
+        images.append(img)
+    
 
-# Filtering keypoints:
-matches = [kp_1[m[0].queryIdx].pt + kp_2[m[0].trainIdx].pt for m in good]
+    return images[0], images[1:]
 
-# Showing both images with matches and correspondences
-matched_image = cv2.drawMatchesKnn(img1, kp_1, img2, kp_2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-cv2.imshow("matched_image", matched_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def imgs_feature_matching(img1,img2, info=False):
+
+    img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    # SIFT keypoints/descriptors:
+    sift = cv2.SIFT_create()
+    kp_1, des_1 = sift.detectAndCompute(img1_gray,None)
+    kp_2, des_2 = sift.detectAndCompute(img2_gray,None)
+
+    # Matching keypoints:
+    matcher = cv2.BFMatcher()
+    matches = matcher.knnMatch(des_1, des_2, k=2)
+
+    # Filtering matches:
+    good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append([m])
+
+    # Filtering keypoints:
+    matches = np.array([kp_1[m[0].queryIdx].pt + kp_2[m[0].trainIdx].pt for m in good])
+
+    # Showing both images with matches and correspondences
+    matched_image = cv2.drawMatchesKnn(img1, kp_1, img2, kp_2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    
+    if info:
+        cv2.imshow("matched_image", matched_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return matches
+
+def warpTwoImages(img1, img2, H):
+    '''warp img2 to img1 with homograph H'''
+    h1,w1 = img1.shape[:2]
+    h2,w2 = img2.shape[:2]
+    pts1 = np.float32([[0,0],[0,h1],[w1,h1],[w1,0]]).reshape(-1,1,2)
+    pts2 = np.float32([[0,0],[0,h2],[w2,h2],[w2,0]]).reshape(-1,1,2)
+    pts2_ = cv2.perspectiveTransform(pts2, H)
+    pts = np.concatenate((pts1, pts2_), axis=0)
+    [xmin, ymin] = np.int32(pts.min(axis=0).ravel() - 0.5)
+    [xmax, ymax] = np.int32(pts.max(axis=0).ravel() + 0.5)
+    t = [-xmin,-ymin]
+    Ht = np.array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) # translate
+
+    warped_image = cv2.warpPerspective(img2, Ht.dot(H), (xmax-xmin, ymax-ymin))
 
 
-# Here we calculate H with ransac:
-h = ransac_homography(matches, n_samples=4, tolerance=1, max_iterations=5000, threshold=0.9)
-
-# Warping image 1 to stich it later.
-img_warped = cv2.warpPerspective(img1, h, (img2.shape[1],img2.shape[0]))
-
-alpha = 0.4
-result = cv2.addWeighted(img_warped, alpha, img2, 1-alpha, 0)
+    blank_image = np.zeros(warped_image.shape, np.uint8)
+    blank_image[t[1]:h1+t[1],t[0]:w1+t[0]] = img1
 
 
-# Putting together all the images so they can be shown with just one winow
-hor1 = np.concatenate((img1, img2), axis=1) 
-hor2 = np.concatenate((img_warped, result), axis=1) 
-subplot = np.concatenate((hor1,hor2), axis=0)
+    img_gray = cv2.cvtColor(blank_image, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(img_gray, 0,255, type=cv2.THRESH_BINARY_INV)
 
-subplot=cv2.resize(subplot,None, fx=0.75, fy=0.75)
-cv2.imshow("blend of warpe  d + img2", subplot)
+    warped_image = cv2.bitwise_and(warped_image, warped_image, mask=mask)
 
-# Creating panoramic image with img1 + img2.
-result2 = cv2.warpPerspective(img1, h, (img1.shape[1] + img2.shape[1], int(img1.shape[0]*1.25)))
-result2[0:img2.shape[0], 0:img2.shape[1]] = img2
-cv2.imshow("Panoramic img1 + img2", result2)
+    result = cv2.add(blank_image, warped_image)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return result
+
+def get_information_cv2Homography(matches):
+
+    # First obtain the homography from cv2.
+    matches_src, matches_dst = np.split(np.array(matches), 2, 1)
+    h, mask = cv2.findHomography(matches_src, matches_dst, cv2.RANSAC)
+
+    mask = np.array([bool(m) for m in mask]) # Change mask from 0-1 to False-True.
+
+    # Next obtain the proyection for each source point and calculate the error.
+    n_inliers = len(mask[mask])
+
+    src, dst = np.split(np.array(matches).transpose(), 2, 0)     
+    src = np.insert(src,2,1,axis=0)     # Adding 1s at the end of the point (2D -> 3D point)
+    dst = np.insert(dst,2,1,axis=0)
+
+    src_proy = np.dot(h,src)            # Multiplying all the 3D points for the homography.
+    src_proy /= src_proy[-1]            # Normalizing proyected points.
+
+    errors = np.linalg.norm(dst-src_proy, axis=0) # Errors: distance between destination position and proyected position.
+    
+    error_inliers = errors[mask]
+
+    mse = np.square(error_inliers).mean()
+    
+    print(f"[#] Result h2: {n_inliers}/{len(matches)}: {n_inliers/len(matches):.3f} ratio. MSE: {mse:.3f}\n")
+
+    return h
+
+
+
+if __name__ == "__main__":
+
+    base_image, images = read_images("media5/")
+
+
+    for image in images[:]:
+        matches = imgs_feature_matching(image, base_image, info=False)
+
+        # Here we calculate H with ransac:
+        h1 = ransac_homography(matches, n_samples=4, tolerance=1, max_iterations=2100, threshold=0.6, info=True)
+
+        h2 = get_information_cv2Homography(matches)
+
+        base_image = warpTwoImages(base_image, image, h1)
+
+        #print(f"{h1 - h2}")
+
+
+    # Resize and show the final result:
+    scale = 700/base_image.shape[0]
+    base_image = cv2.resize(base_image, None, fx=scale, fy=scale)
+    cv2.imshow("Panoramic img1 + img2", base_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- ransac.py
+++ ransac.py
@@ -1,73 +1,115 @@
 import numpy as np, random
 
-def get_homography(src, dst):
+def get_homography(samples):
+
+    src, dst = np.split(np.array(samples), 2, 1)
 
     A = []
     N = len(src) # A matrix can be ajusted for N_points >= 4.
 
     for i in range(N):
-        x, y = src[i][0], src[i][1]
-        xp, yp = dst[i][0], dst[i][1]
-        A.append([x, y, 1, 0, 0, 0, -x * xp, -xp * y, -xp])
-        A.append([0, 0, 0, x, y, 1, -yp * x, -yp * y, -yp])
+        xs, ys  = src[i]
+        xd, yd  = dst[i]
+
+        A.append([-xs, -ys, -1,  0 ,  0 ,  0, xs * xd, xd * ys, xd])
+        A.append([0  ,  0 ,  0, -xs, -ys, -1, yd * xs, yd * ys, yd])
 
     A = np.asarray(A)
 
-    U, S, Vh = np.linalg.svd(A)
-    L = Vh[-1, :] / Vh[-1, -1]
+    U, S, Vh = np.linalg.svd(A) # Calculate SVD of A to solve A*h = 0. Whithin the last row of V will be H.
 
-    H = L.reshape(3, 3)
-    H /= H[2, 2]            # Normalization step. H is [3, 3] matrix.
+    H = np.reshape(Vh[8], (3, 3)) # Reshape the min singular value into a 3 by 3 matrix
+
+    H = H / H.item(8) # Normalize to get H
 
     return H
 
-def ransac_homography(matches, n_samples=4, tolerance=2, max_iterations=500, threshold=1):
+def is_non_collinear(points_matrix):
+    p = np.append(points_matrix, points_matrix[:2],axis=0)
+
+    for i in range(4):
+        temp = p[i:3+i]
+
+        rank = np.linalg.matrix_rank(temp)
+        if rank < 2: 
+            return False
+
+    return True
+
+def ransac_homography(matches, n_samples=4, tolerance=1, max_iterations=500, threshold=1, info=False):
 
     best_inliers = 0
     best_H = np.eye(3)
     best_MSE = np.inf
 
+    best_errors = []
+
     # separate source points from destination points.
-    matches_src, matches_dst = np.split(np.array(matches).transpose(), 2, 0)     
-    matches_src = np.insert(matches_src,2,1,axis=0)     # Adding 1 at the end and reshaping to columns instead of rows.
-    matches_dst = np.insert(matches_dst,2,1,axis=0) 
+    matches_src, matches_dst = np.split(matches.transpose(), 2, 0)     
+    matches_src = np.insert(matches_src,2,1,axis=0)     # Adding 1s at the end of the point (2D -> 3D point)
+    matches_dst = np.insert(matches_dst,2,1,axis=0)     # and reshaping points in columns instead of rows.
 
 
-    for i in range(max_iterations):
+    for i in range(max_iterations): 
 
         # First we get n random samples from all the matches.
-        samples = np.array(random.sample(matches, n_samples))
-        samp_src, samp_dst = np.split(samples, 2, 1)
+        samples = random.sample(matches.tolist(), n_samples)
 
-        H = get_homography(samp_src,samp_dst)   # Obtaining the Homography matrix.                        
+        H = get_homography(samples)   # Obtaining the Homography matrix.                        
 
         # Proyecting all the source points into the destination space using the Homography.
         src_proy = np.dot(H,matches_src)
+        src_proy /= src_proy[-1]            # Normalizing the points.
 
         # Obtaining the distances between all the proyected points and their correspondences. 
-        distances = np.linalg.norm(matches_dst-src_proy,axis=0)
+        errors = np.linalg.norm(matches_dst-src_proy, axis=0)
 
-        # Now we the number of inliers for this H and the Mean Squared Error of the distances between matches.
-        error_inliers = distances[distances<=tolerance]
+        # Now we the number of inliers for this H and the Mean Squared Error of the error between matches.
+        error_inliers = errors[errors<=tolerance]
         n_inliers = len(error_inliers)
+
         if n_inliers:
             mse = np.square(error_inliers).mean()
         else:
             mse = np.inf
         
         if n_inliers > best_inliers or (n_inliers == best_inliers and mse < best_MSE):
+            best_errors = errors <= tolerance
             best_inliers = n_inliers
-            best_H = H
             best_MSE = mse
+            best_H = H
 
-        if (i+1) % 500 == 0:
+
+        if info and (i+1) % 2000 == 0:
             print(f"-> i: {i+1} \tbest: [{best_inliers}/{len(matches)}] \tMSE: {best_MSE:.3f}")
 
         if best_inliers/len(matches) >= threshold:
             break
 
+    ### Now the homography is refined by recalculating it but this time with all the inliers:
 
-    print(f"\n[#]: n of iterations done: {i+1}.")
-    print(f"[#] Result: {best_inliers}/{len(matches)}: {best_inliers/len(matches):.3f} ratio. MSE: {best_MSE:.3f}\n")
+    inliers = matches[best_errors] # Getting only the inliers.
 
-    return best_H
+    refined_H = get_homography(inliers)   # Obtaining the Homography matrix with all the inliers.                        
+
+    # Proyecting all the source points into the destination space using the Homography.
+    src_proy = np.dot(refined_H, matches_src)
+    src_proy /= src_proy[-1]                    # Normalizing the points.
+
+    # Obtaining the distances between all the proyected points and their correspondences. 
+    errors = np.linalg.norm(matches_dst-src_proy, axis=0)
+
+    # Number of inliers for this refined_H and the Mean Squared Error of the distances between matches.
+    error_inliers = errors[errors<=tolerance]
+    refined_n_inliers = len(error_inliers)
+    refined_mse = np.square(error_inliers).mean()
+
+
+    if info:
+        print(f"\n[#]: n of iterations done: {i+1}.")
+        print(f"[#] Result: {best_inliers}/{len(matches)}: {best_inliers/len(matches):.3f} ratio. MSE: {best_MSE:.3f}")
+        print(f"[#] Refined result: {refined_n_inliers}/{len(matches)}: {refined_n_inliers/len(matches):.3f} ratio. MSE: {refined_mse:.3f}\n")
+
+ 
+
+    return refined_H
